b259636a5cf10a55d440386316ef2c891708a071
Ross Bille <rossjbille@gmail.com>
2014-04-09 23:19:12 +1000

    moved initialisation of virtual environments to a common place

--- python-venv-setup.sh
+++ python-venv-setup.sh
@@ -1,18 +1,6 @@
 #!/bin/bash 
 
-#python venv setup 
-#download venv
-curl -Lo virtualenv-tmp.tar.gz 'https://github.com/pypa/virtualenv/tarball/master'
-#untar to a temp directory
-mkdir temp
-tar -C ./temp -xzf virtualenv-tmp.tar.gz
-#rename
-mv temp/* temp/pypa
-#move into temp
-cd temp
-#setup the venv
-python pypa/virtualenv.py venv
-#move back up
-cd ..
-#cleanup
-rm virtualenv-tmp.tar.gz 
+# check for input parameter
+((!$#)) && echo No arugments supplied! && exit 1
+#initialise python venv 
+virtualenv /Development/python/virtualenvironments/$1
