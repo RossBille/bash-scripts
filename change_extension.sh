2d946fb36302c4ffa56742b15fcc50b831a1dd19
Ross Bille <rossjbille@gmail.com>
2014-06-17 12:37:19 +1000

    [DOC] commented change_extension.sh

--- change_extension.sh
+++ change_extension.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+#if the first argument is empty then default original_ext to jar
 if [ -z "$1" ] 
 then
     original_ext="jar"
 else
 	original_ext=$1
 fi
+#if the first argument is empty then default new_ext to zip
 if [ -z "$2" ] 
 then
     new_ext="zip"
